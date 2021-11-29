0d4dae6f5f10b56868301d7ca257a9520c2d7c1a
Emir Erten <erte0004@algonquinlive.com>
2021-11-29 16:47:20 -0500

    pseudo code

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// ---pseudo code
+// -Createa Form
+//     Title Section
+//     Dropbox - DYNAMICALLY ADDED JS
+//         Year
+//         Month
+//         Day
+//     Submit Button 
+// -Calc the Date from epoch date
+// -Display a countdown to the selected date
+//     Days
+//     Hours
+//     Minutes
+//     Seconds
+// -Local Storage the selected date
+// -Open page take local storage data and put calc back into function
+// -Button to take user back to form 
+// -Page refresh Calc Automatically starts
